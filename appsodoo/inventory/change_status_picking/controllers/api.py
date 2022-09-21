4d1a47d3a35c1d92b7f01df227f6d3e4439eb6be
agustinuspangestu38@gmail.com <agustinuspangestu38@gmail.com>
2022-09-21 16:39:14 +0700

    [fix] authenticate and response api
    (cherry picked from commit fcc3c02c690a82ac7f2a83a79ff34e8ad5c046b0)

--- appsodoo/inventory/change_status_picking/controllers/api.py
+++ appsodoo/inventory/change_status_picking/controllers/api.py
@@ -8,10 +8,18 @@ class ApiController(http.Controller):
     def create_transit_log(self, **kwargs):
         """
         REST API POST for create table transit_log
+
+        `system will be execute authenticate when kwargs['id_user'] is None`
         """
         request.env.cr.savepoint()
 
         try:
+            if not kwargs.get('id_user'):
+                self.authenticate(kwargs.get('db'), kwargs.get('login'), kwargs.get('password'), kwargs.get('base_location'))
+
+            # hapus parameter untuk execute auth
+            kwargs = self.clear_param_auth(kwargs)
+
             # value kwargs ditampung ke temp_kwargs untuk proses create `transit_log`
             temp_kwargs = copy.deepcopy(kwargs)
             for d in temp_kwargs:
@@ -19,44 +27,50 @@ class ApiController(http.Controller):
                 if type(temp_kwargs[d]) == list:
                     temp_kwargs[d] = [(0,0, dl) for dl in temp_kwargs[d]]
 
-            request.env['transit.log'].sudo().create(temp_kwargs)
+            res = request.env['transit.log'].sudo().create(temp_kwargs)
             request.env.cr.commit()
 
             return {
                 'status': 200,
-                'message': 'success',
-                'response': kwargs
+                'message': 'success'
             }
 
         except Exception as e:
 
             request.env.cr.rollback()
             return {
-                'error': "Error",
+                'message': "Error",
                 'error-descrip': str(e)
             }
 
     @http.route(['/api/update_status_picking/'], type="json", auth="public", method=['POST'], csrf=False)
     def update_status_picking(self, **kwargs):
         """
-        REST API POST for create table transit_log
+        REST API POST for update state pada table `stock_picking`
+
+        `system will be execute authenticate when kwargs['id_user'] is None`
         """
         request.env.cr.savepoint()
 
         try:
+            if not kwargs.get('id_user'):
+                self.authenticate(kwargs.get('db'), kwargs.get('login'), kwargs.get('password'), kwargs.get('base_location'))
+
+            # hapus parameter untuk execute auth
+            kwargs = self.clear_param_auth(kwargs)
+
             if kwargs.get('state') in ['in_transit', 'delivered']:
-                request.env['stock.picking'].sudo().search(
+                res = request.env['stock.picking'].sudo().search(
                     [('id', '=', kwargs['picking_id'])]).write({'state': kwargs['state']})
                 request.env.cr.commit()
 
                 return {
                     'status': 200,
-                    'message': 'success',
-                    'response': kwargs
+                    'message': 'success'
                 }
             else:
                 return {
-                    'error': "Error",
+                    'message': "Error",
                     'error-descrip': str('can only update state to (in_transit / delivered)')
                 }
 
@@ -68,4 +82,15 @@ class ApiController(http.Controller):
                 'error-descrip': str(e)
             }
 
-    
+    @http.route('/web/session/authenticate', type='json', auth="public")
+    def authenticate(self, db, login, password, base_location=None):
+        request.session.authenticate(db, login, password)
+        return request.env['ir.http'].session_info()
+
+    def clear_param_auth(self, kwargs):
+        if 'id_user' in kwargs: del kwargs['id_user']
+        if 'db' in kwargs: del kwargs['db']
+        if 'login' in kwargs: del kwargs['login']
+        if 'password' in kwargs: del kwargs['password']
+        if 'base_location' in kwargs: del kwargs['base_location']
+        return kwargs
