443f3998fbb00e7d21698dccf208e5e71b1584fe
agustinuspangestu38@gmail.com <agustinuspangestu38@gmail.com>
2022-09-30 03:41:26 +0700

    [feat] adjust column and filter

--- appsodoo/inventory/pilarmedia_for_compotec/models/wholesale_job_report.py
+++ appsodoo/inventory/pilarmedia_for_compotec/models/wholesale_job_report.py
@@ -16,43 +16,46 @@ class WholesaleJobReport(models.Model):
     product_ids = fields.Many2one('product.product', string='Product')
     user_ids = fields.Many2one('employee.custom', string='User')
     factor = fields.Float(string='Factor')
-    total_lot = fields.Float(string='Total Lot')
     total_ng = fields.Float(string='Total NG')
     total_ok = fields.Float(string='Total OK')
-    total_ok_ng = fields.Float(string='Total OK + NG')
+    biggest_lot = fields.Many2one('lot', string="Last Lot")
+    total_set = fields.Float(string='Total Set')
+    total_pcs= fields.Float(string='Total Pcs')
     checked_coordinator = fields.Many2one('employee.custom', string='Checked Coordinator')
     checked_qc = fields.Many2one('employee.custom', string='Checked QC')
 
     def init(self):
         self.env.cr.execute("""
-        DROP FUNCTION IF EXISTS wholesale_job_report(DATE, DATE);
-        CREATE OR REPLACE FUNCTION wholesale_job_report(date_start DATE, date_end DATE)
+        DROP FUNCTION IF EXISTS wholesale_job_report(DATE, DATE, CHAR, CHAR, CHAR);
+        CREATE OR REPLACE FUNCTION wholesale_job_report(date_start DATE, date_end DATE, company CHAR, job CHAR, shift CHAR)
         RETURNS VOID AS $BODY$
         DECLARE
             
             csr cursor for
             SELECT 
-                wj.id as wj_id, wj.date, j.id as job_id, wjl.product_ids as product_id, u.id as user_id, wjl.total_lot, wjl.total_ng, 
-                wjl.total_ok, wjl.factor, wj.checked_coordinator, wj.checked_qc, wjl.factor, wjl.total_ok_ng
+                wj.id as wj_id, wj.date, j.id as job_id, wjl.product_ids as product_id, u.id as user_id, wjl.total_set, wjl.total_ng, 
+                wjl.total_ok, wjl.factor, wj.checked_coordinator, wj.checked_qc, wjl.factor, wjl.total_pcs, wjl.biggest_lot
             FROM wholesale_job wj
             LEFT JOIN wholesale_job_line wjl ON wjl.wholesale_job_ids = wj.id
-            LEFT JOIN wholesale_job_lot_line wjll ON wjll.wholesale_job_line_ids = wjl.id
             LEFT JOIN job j ON j.id = wj.job_ids
             -- LEFT JOIN product_product p ON p.id = wjl.product_ids
             -- LEFT JOIN product_template pt ON pt.id = p.product_tmpl_id
             LEFT JOIN res_partner u ON u.id = wjl.user_ids
-            WHERE wj.date BETWEEN date_start AND date_end
+            WHERE 
+                wj.date BETWEEN date_start AND date_end AND 
+                    IF(job IS NOT NULL, CAST(j.id AS TEXT) = job, TRUE) AND 
+                    IF(shift IS NOT NULL, CAST(wj.shift AS TEXT) = shift, TRUE)
             ORDER BY wj.date;
 
         BEGIN
             delete from wholesale_job_report;
             
             for rec in csr loop
-                insert into wholesale_job_report (id_wj, date, job_ids, product_ids, user_ids, total_lot, 
-                    total_ng, total_ok, checked_coordinator, checked_qc, factor, total_ok_ng) 
+                insert into wholesale_job_report (id_wj, date, job_ids, product_ids, user_ids, total_set, 
+                    total_ng, total_ok, checked_coordinator, checked_qc, factor, total_pcs, biggest_lot) 
                     values (rec.wj_id, rec.date, rec.job_id, rec.product_id, rec.user_id, 
-                        rec.total_lot, rec.total_ng, rec.total_ok, rec.checked_coordinator, 
-                        rec.checked_qc, rec.factor, rec.total_ok_ng);
+                        rec.total_set, rec.total_ng, rec.total_ok, rec.checked_coordinator, 
+                        rec.checked_qc, rec.factor, rec.total_pcs, rec.biggest_lot);
 	        end loop;
         END;
 
--- appsodoo/inventory/pilarmedia_for_compotec/wizard/wholesale_job_report_wizz.py
+++ appsodoo/inventory/pilarmedia_for_compotec/wizard/wholesale_job_report_wizz.py
@@ -3,12 +3,22 @@ from odoo import models, fields, api
 
 class WholesaleJobReport(models.TransientModel):
     _name = "wholesale.job.report.wizzard"
-    date_start = fields.Date(string='Date Start')
-    date_end = fields.Date(string='Date End')
+    date_start = fields.Date(string='Date Start', required=True)
+    date_end = fields.Date(string='Date End', required=True)
+    company = fields.Many2one('res.company', string='Company')
+    job = fields.Many2one('job', string='Job')
+    shift = fields.Many2one('shift', string='Shift')
 
     def call_wholesale_job_report(self):
         cr = self.env.cr
-        cr.execute("select wholesale_job_report(%s,%s)",(self.date_start, self.date_end))
+
+        company = str(self.company.id) if self.company else ''
+        job = str(self.job.id) if self.job else ''
+        shift = str(self.shift.id) if self.shift else ''
+
+        cr.execute("select wholesale_job_report(%s,%s,%s,%s,%s)",(
+            self.date_start, self.date_end, company, job, shift
+        ))
         waction = self.env.ref("pilarmedia_for_compotec.""wholesale_job_report_action")
         result = waction.read()[0]
         return result
