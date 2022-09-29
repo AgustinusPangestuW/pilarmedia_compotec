606ef0848c09f0ee3b036cfa50b447408af9e96c
agustinuspangestu38@gmail.com <agustinuspangestu38@gmail.com>
2022-09-29 13:10:21 +0700

    [fix] variable name & calculation total OK for type SET

--- appsodoo/inventory/pilarmedia_for_compotec/models/wholesale_job.py
+++ appsodoo/inventory/pilarmedia_for_compotec/models/wholesale_job.py
@@ -20,10 +20,17 @@ class WholesaleJob(models.Model):
     name = fields.Char(string='Wholesale Job Name', required=True, copy=False, readonly=True, index=True, default=lambda self: _('New'))
     sequence = fields.Integer(string='Sequence', default=10)
     date = fields.Date(string="Date", required=True, default=datetime.now().date())
-    job_ids = fields.Many2one('job', string='Job', required=True, domain=[('active', '=', 1)])
+    job_ids_active = fields.Boolean(string='Job ID Active', compute="_set_job_id_active", store=False)
+    job_ids = fields.Many2one(
+        'job', 
+        string='Job', 
+        domain=[('active', '=', 1)], 
+        required=True
+    )
     lot_lines = fields.One2many('wholesale.job.line', 'wholesale_job_ids', 'Lot Line', auto_join=True)
     checked_coordinator = fields.Many2one('employee.custom', string='Checked Coordinator')
     checked_qc = fields.Many2one('employee.custom', string='Checked QC')
+    shift = fields.Many2one('shift', string='Shift')
 
     @api.model
     def create(self, vals):
@@ -39,6 +46,12 @@ class WholesaleJob(models.Model):
 
         return super().create(vals)  
 
+    @api.depends('lot_lines')
+    def _set_job_id_active(self):
+        for rec in self:
+            if len(rec.lot_lines) > 0: rec.job_ids_active = 1
+            else: rec.job_ids_active = 0
+
 
 class WholesaleJobLine(models.Model):
     _name = "wholesale.job.line"
@@ -47,21 +60,39 @@ class WholesaleJobLine(models.Model):
     sequence = fields.Integer(string='Sequence')
     wholesale_job_ids = fields.Many2one(
         'wholesale.job', 
-        'Lot ID', 
+        'wholesale job ids', 
         index=1, 
         ondelete="cascade", 
         copy=False
     )
-    is_check = fields.Boolean(string='Is Check')
-    job_ids = fields.Many2one('job', string='Job', required=True, domain=[('active', '=', 1)])
-    product_ids = fields.Many2one('product.product', string='Produk')
+    is_set = fields.Boolean(
+        string='Is Set', 
+        readonly=True, 
+        compute="_get_detail_product", 
+        store=True,
+        help="This field will be True when product UOM = `set`"
+    )
+    job_ids = fields.Many2one(
+        'job', string='Job', 
+        required=True, 
+        readonly=True, 
+        domain=[('active', '=', 1)]
+    )
+    product_ids = fields.Many2one('product.product', string='Produk', required=True)
     user_ids = fields.Many2one('employee.custom', string='Nama', required=True)
-    total_lot = fields.Float(string="Total Lot", readonly=True, compute="_calc_total_lot", store=True)
-    total_ng = fields.Float(string="Total NG", readonly=True, compute="_calc_total_ng_ok", store=True)
+    total_set = fields.Float(string="Total SET", readonly=True, compute="_calc_total_set", store=True)
+    total_ng = fields.Float(string="Total NG", compute="_calc_total_ng_ok", store=True)
     total_ok = fields.Float(string="Total OK", readonly=True, compute="_calc_total_ng_ok", store=True)
-    total_ok_ng = fields.Float(string='Total OK & NG', readonly=True, compute="_calc_total_ng_ok", store=True)
+    total_pcs = fields.Float(string='Total PCS', readonly=True, compute="_calc_total_ng_ok", store=True)
     factor = fields.Float(string='Factor')
-    biggest_lot = fields.Many2one('lot', string='Last Lot ID', readonly=True, compute="_compute_get_biggest_lot", store=True)
+    biggest_lot = fields.Many2one(
+        'lot', 
+        string='Last Lot', 
+        readonly=True, 
+        compute="_compute_get_biggest_lot", 
+        store=True, 
+        help="this field for track last Lot ID"
+    )
     wholesale_job_lot_lines = fields.One2many('wholesale.job.lot.line', 'wholesale_job_line_ids', 'Lot Line', auto_join=True)           
 
     @api.model
@@ -107,7 +138,7 @@ class WholesaleJobLine(models.Model):
             'biggest_lot': biggest_lot_id
         })
 
-        self._calc_total_lot()
+        self._calc_total_set()
         self._calc_total_ng_ok()
 
         return self
@@ -126,33 +157,55 @@ class WholesaleJobLine(models.Model):
                 'biggest_lot': biggest_lot
             })
 
-            self._calc_total_lot()
+            self._calc_total_set()
             self._calc_total_ng_ok()
 
             return self
         else:
             raise exceptions.ValidationError(_("cur Job Lot Line is null."))
 
-    @api.depends('is_check', 'wholesale_job_lot_lines.ng', 'wholesale_job_lot_lines.ok')
+    @api.depends('is_set', 'wholesale_job_lot_lines.ng', 'wholesale_job_lot_lines.ok', \
+        'total_set', 'total_ng')
     def _calc_total_ng_ok(self):
-        total_ng, total_ok = 0, 0
         for rec in self:
-            for line in self.wholesale_job_lot_lines:
-                if not rec.is_check:
+            # calculation NG & OK when is_set = FALSE (NON set)
+            if not rec.is_set:
+                self._calculate_ok_ng_pcs()
+
+            # calculation NG & OK when is_set = TRUE
+            else:
+                self._calc_ok()
+
+
+    def _calculate_ok_ng_pcs(self):
+        total_ng, total_ok, total_pcs = 0, 0, 0
+        for rec in self:
+            if not rec.is_set:
+                for line in self.wholesale_job_lot_lines:
                     total_ng += line.ng
                     total_ok += line.ok
+                    total_pcs = total_ng + total_ok
 
-            rec.update({
-                'total_ng': total_ng,
-                'total_ok': total_ok,
-                'total_ok_ng': total_ok + total_ng
-            }) 
+                rec.update({
+                    'total_ng': total_ng,
+                    'total_ok': total_ok,
+                    'total_pcs': total_pcs
+                }) 
+
+    @api.onchange('total_ng', 'total_set')
+    def _calc_ok(self):
+        for rec in self:
+            # calculation NG & OK when is_set = TRUE
+            if rec.is_set:
+                rec.update({
+                    'total_ok': rec.total_set - rec.total_ng
+                })
             
-    @api.depends('factor', 'is_check')
-    def _calc_total_lot(self):
+    @api.depends('factor', 'is_set')
+    def _calc_total_set(self):
         new_total_lot = 0
         for rec in self:
-            if rec.is_check:
+            if rec.is_set:
                 last_lot_list = rec.wholesale_job_lot_lines
                 last_lot_name = ''
                 if len(last_lot_list) > 0:
@@ -160,8 +213,18 @@ class WholesaleJobLine(models.Model):
                     new_total_lot = int(rec.factor) * int(last_lot_name)
             else:
                 new_total_lot = 0
-        rec.total_lot = new_total_lot
-                
+        rec.total_set = new_total_lot
+
+    @api.depends('product_ids')
+    def _get_detail_product(self):
+        new_is_set = 0
+        for rec in self:
+            if rec.product_ids: 
+                uom = rec.product_ids.product_tmpl_id.uom_id.name
+                if uom.lower() == "set":
+                    new_is_set = 1
+            
+            rec.is_set = new_is_set
 
 class WholesaleJobLotLine(models.Model):
     _name = "wholesale.job.lot.line"
