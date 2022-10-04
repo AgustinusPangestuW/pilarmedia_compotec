ead694a2eb23b2e158d69ddf2aede73a05995295
agustinuspangestu38@gmail.com <agustinuspangestu38@gmail.com>
2022-10-04 11:59:56 +0700

    [feat] domain for field vendor
    * agar option yang tampil hanya HBR, ACP, COMPO, dll

--- appsodoo/inventory/pilarmedia_for_compotec/models/employee_custom.py
+++ appsodoo/inventory/pilarmedia_for_compotec/models/employee_custom.py
@@ -5,4 +5,9 @@ class EmployeeCustom(models.Model):
 
     name = fields.Char(string='Nama Pegawai', required=True)
     position = fields.Char(string='Jabatan')
-    vendor = fields.Many2one('res.partner', string='Vendor Subcon')
+    vendor = fields.Many2one(
+        'res.partner', 
+        string='Vendor Subcon', 
+        domain=[('is_subcon', '=', 1)], 
+        help="relation with res parner, with is_subcon = 1"
+    )
