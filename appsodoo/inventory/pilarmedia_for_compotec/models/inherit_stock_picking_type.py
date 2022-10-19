1792b90b099c19fe6314d8e548da2f0aa5742956
Agustinus Pangestu Wijaya <agustinuspangestu38@gmail.com>
2022-10-19 08:23:16 +0000

    [feat] field is_subcon needed for auto create Stock Picking step2

--- appsodoo/inventory/pilarmedia_for_compotec/models/inherit_stock_picking_type.py
+++ appsodoo/inventory/pilarmedia_for_compotec/models/inherit_stock_picking_type.py
@@ -2,6 +2,8 @@ from odoo import models, fields, api, _
 
 class InheritStockPickingType(models.Model):
     _inherit = 'stock.picking.type'
+
+    is_subcon = fields.Boolean(string='Is Subcontracting Transfer')
         
     def _get_action(self, action_xmlid):
         action = super()._get_action(action_xmlid)
