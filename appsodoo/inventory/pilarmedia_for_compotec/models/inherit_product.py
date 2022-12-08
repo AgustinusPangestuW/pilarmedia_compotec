9a2a0a6f1ec9783ade29e94ad346ec5b8a476cf2
agustinuspangestu38@gmail.com <agustinuspangestu38@gmail.com>
2022-12-08 11:16:09 +0700

    [fix] edit into `stock.group_stock_manager`

--- appsodoo/inventory/pilarmedia_for_compotec/models/inherit_product.py
+++ appsodoo/inventory/pilarmedia_for_compotec/models/inherit_product.py
@@ -4,9 +4,9 @@ from odoo.exceptions import UserError
 class InheritProduct(models.Model):
     _inherit = 'product.product'
 
-    qty_available = fields.Float(groups="group_stock_manager")
-    virtual_available = fields.Float(groups="group_stock_manager")
-    standard_price = fields.Float(groups="group_stock_manager")
-    lst_price  = fields.Float(groups="group_stock_manager")
+    qty_available = fields.Float(groups="stock.group_stock_manager")
+    virtual_available = fields.Float(groups="stock.group_stock_manager")
+    standard_price = fields.Float(groups="stock.group_stock_manager")
+    lst_price  = fields.Float(groups="stock.group_stock_manager")
 
     
