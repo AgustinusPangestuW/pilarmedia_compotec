80754f64965a9407023ebc55eb9a1d0c6709707a
agustinuspangestu38@gmail.com <agustinuspangestu38@gmail.com>
2022-12-08 11:13:31 +0700

    [fix] edit into `group_stock_manager`

--- appsodoo/inventory/pilarmedia_for_compotec/models/inherit_product.py
+++ appsodoo/inventory/pilarmedia_for_compotec/models/inherit_product.py
@@ -4,9 +4,9 @@ from odoo.exceptions import UserError
 class InheritProduct(models.Model):
     _inherit = 'product.product'
 
-    qty_available = fields.Float(groups="stock.group_stock_user")
-    virtual_available = fields.Float(groups="stock.group_stock_user")
-    standard_price = fields.Float(groups="stock.group_stock_user")
-    lst_price  = fields.Float(groups="stock.group_stock_user")
+    qty_available = fields.Float(groups="group_stock_manager")
+    virtual_available = fields.Float(groups="group_stock_manager")
+    standard_price = fields.Float(groups="group_stock_manager")
+    lst_price  = fields.Float(groups="group_stock_manager")
 
     
--- appsodoo/inventory/pilarmedia_for_compotec/models/inherit_product_template.py
+++ appsodoo/inventory/pilarmedia_for_compotec/models/inherit_product_template.py
@@ -5,7 +5,7 @@ class InheritProductTemplate(models.Model):
     _inherit = 'product.template'
 
     pocket_factor = fields.Integer(string='Kantong', help="this field for default value when fetch in checksheet borongan.")
-    qty_available = fields.Float(groups="stock.group_stock_user")
-    virtual_available = fields.Float(groups="stock.group_stock_user")
-    standard_price = fields.Float(groups="stock.group_stock_user")
-    lst_price  = fields.Float(groups="stock.group_stock_user")
+    qty_available = fields.Float(groups="group_stock_manager")
+    virtual_available = fields.Float(groups="group_stock_manager")
+    standard_price = fields.Float(groups="group_stock_manager")
+    lst_price  = fields.Float(groups="group_stock_manager")
