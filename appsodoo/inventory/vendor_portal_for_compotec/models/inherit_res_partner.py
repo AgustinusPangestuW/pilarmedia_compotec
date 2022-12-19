4c9d2a463d0d486f95e989be1a232a6801622835
agustinuspangestu38@gmail.com <agustinuspangestu38@gmail.com>
2022-12-20 06:35:32 +0700

    [feat] field code & is_approve

--- appsodoo/inventory/vendor_portal_for_compotec/models/inherit_res_partner.py
+++ appsodoo/inventory/vendor_portal_for_compotec/models/inherit_res_partner.py
@@ -4,6 +4,8 @@ from odoo.exceptions import UserError
 class InheritResPartner(models.Model):
     _inherit = 'res.partner'
 
+    code = fields.Char(string='Code', copy=False)
+    is_approve = fields.Boolean(string='Approve?', copy=False, default=1)
     with_confirm_date = fields.Boolean(string='With Confirmation Date ?', default=1)
     npwp = fields.Char(string='NPWP')
     npwp_name = fields.Char(string='Nama NPWP')
@@ -13,4 +15,8 @@ class InheritResPartner(models.Model):
         relation='prod_categ_res_partner',
         string='Product Categories'
     )
+
+    _sql_constraints = [
+        ('vendor_code_uniq', 'unique(code)', 'Code Vendor must be unique!'),
+    ]
    
