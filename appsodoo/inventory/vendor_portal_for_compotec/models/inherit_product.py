606b6160a24e1cd1d5dfa9a4658acdde09ef0433
agustinuspangestu38@gmail.com <agustinuspangestu38@gmail.com>
2022-12-23 12:52:44 +0700

    [fix] remove auto false `sale_ok` & `purchase_ok`

--- appsodoo/inventory/vendor_portal_for_compotec/models/inherit_product.py
+++ appsodoo/inventory/vendor_portal_for_compotec/models/inherit_product.py
@@ -17,16 +17,6 @@ class InheritProductTemplate(models.Model):
     # standard_price = fields.Float(groups="stock.group_stock_manager")
     # lst_price  = fields.Float(groups="stock.group_stock_manager")
 
-    def fill_base_on_partner(self):
-        user = self.env.user
-        if user and user.partner_id.supplier_rank:
-            return 0
-        else: 
-            return 1
-
-    sale_ok = fields.Boolean('Can be Sold', default=fill_base_on_partner)
-    purchase_ok = fields.Boolean('Can be Purchased', default=fill_base_on_partner)
-
     @api.depends('seller_ids')
     def get_vendor_from_seller_ids(self):
         for rec in self:
@@ -54,16 +44,6 @@ class InheritProduct(models.Model):
     # virtual_available = fields.Float(groups="stock.group_stock_manager")
     # standard_price = fields.Float(groups="stock.group_stock_manager")
     # lst_price  = fields.Float(groups="stock.group_stock_manager")
-
-    def fill_base_on_partner(self):
-        user = self.env.user
-        if user and user.partner_id.is_approve:
-            return 1
-        else: 
-            return 0
-
-    sale_ok = fields.Boolean('Can be Sold', default=fill_base_on_partner)
-    purchase_ok = fields.Boolean('Can be Purchased', default=fill_base_on_partner)
 
     @api.depends('seller_ids')
     def get_vendor_from_seller_ids(self):
